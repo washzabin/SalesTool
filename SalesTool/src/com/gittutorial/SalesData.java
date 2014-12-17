af2db24f8b269a5579a21930f4e3df313abb00e2
Ashzabin Wadud <ashzabinwadud@gmail.com>
2014-12-17 15:24:49 +0600

    Added the data

--- SalesTool/src/com/gittutorial/SalesData.java
+++ SalesTool/src/com/gittutorial/SalesData.java
@@ -9,7 +9,7 @@ public class SalesData {
 		System.out.println("TODO: Implement data display.");
 		for(int i = 0; i <= data.length; i++)
 		{
-			
+			System.out.println("Data no." + i + ": " + data[i]);
 		}
 	}
 }
